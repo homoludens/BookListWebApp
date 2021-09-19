2d4b3f8b390ac335b733bb3d5f32dc2ac0838fe8
Marko Djordjevic <marko@droopia.net>
2021-09-19 10:55:14 +0200

    bloc greate working

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Blog;
+use App\Form\Type\BlogFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use App\Repository\BlogRepository;
 
 
 
@@ -20,6 +25,31 @@ class MainController extends AbstractController
    */
   public function index(BlogRepository $blogRepository)
   {
-    return $this->render('list.html.twig');
+    return $this->render('list.html.twig', ['blogs' => $blogRepository->findAll()]);
+  }
+
+  /**
+   * @Route("/create")
+   *
+   * @param Request $request
+   *
+   * @return Response
+   */
+  public function createBlog(Request $request,EntityManagerInterface $entityManager)
+  {
+    $form = $this->createForm(BlogFormType::class, new Blog());
+
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+      $blog = $form->getData();
+      $entityManager->persist($blog);
+      $entityManager->flush();
+      $this->addFlash('success', 'Blog was created!');
+    }
+
+
+    return $this->render('create.html.twig', [
+      'form' => $form->createView()
+    ]);
   }
 }
